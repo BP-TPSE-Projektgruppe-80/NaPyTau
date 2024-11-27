a3766793750b63f9df4de24c56c7080f3612afb3
Benedikt Brunner <122370755+Benedikt-Brunner@users.noreply.github.com>
2024-11-27 23:16:15 +0100

    Add data structures to represent a dataset

---
+++ napytau/ingest/model/relative_velocity.py
@@ -0,0 +1,15 @@
+class RelativeVelocity:
+    velocity: float
+
+    def __init__(self, raw_velocity: float):
+        """raw_velocity: the measure velocity relative to the speed of light"""
+        if raw_velocity < 0:
+            raise ValueError("Velocity cannot be negative")
+
+        if raw_velocity > 1:
+            raise ValueError("Velocity cannot be greater than the speed of light")
+
+        self.velocity = raw_velocity
+
+    def get_velocity(self) -> float:
+        return self.velocity
