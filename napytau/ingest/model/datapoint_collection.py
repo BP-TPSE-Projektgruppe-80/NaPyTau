3ee0268fa5374660ddfe4d9a9b7e112009fc1185
Benedikt Brunner <122370755+Benedikt-Brunner@users.noreply.github.com>
2024-11-28 12:29:15 +0100

    Introduce ValueErrorPair class and refactor datapoint to use it

--- napytau/ingest/model/datapoint_collection.py
+++ napytau/ingest/model/datapoint_collection.py
@@ -1,81 +1,93 @@
-from typing import Dict, List, Tuple
+from __future__ import annotations
+from typing import Dict, List, Callable
 
 from napytau.ingest.model.datapoint import Datapoint
+from napytau.util.coalesce import coalesce
+from napytau.util.model.value_error_pair import ValueErrorPair
 
 
 class DatapointCollection:
-    elements: Dict[float, Datapoint]
+    elements: Dict[int, Datapoint]
 
     def __init__(self, raw_datapoints: List[Datapoint]):
         self.elements = {}
         for datapoint in raw_datapoints:
-            self.elements[datapoint.distance] = datapoint
+            self.elements[hash(datapoint.distance.value)] = datapoint
 
-    def as_list(self) -> Dict[float, Datapoint]:
+    def as_dict(self) -> Dict[int, Datapoint]:
+        """Return the collection as a dictionary. Keys are the hash of the distance value."""
         return self.elements
 
-    def add_datapoint(self, datapoint: Datapoint):
-        self.elements[datapoint.distance] = datapoint
+    def filter(self, filter_func: Callable[[Datapoint], bool]) -> DatapointCollection:
+        return DatapointCollection(list(filter(filter_func, self.elements.values())))
 
-    def get_datapoint_by_distance(self, distance: float) -> Datapoint:
-        return self.elements[distance]
+    def add_datapoint(self, datapoint: Datapoint) -> None:
+        self.elements[hash(datapoint.distance.value)] = datapoint
 
-    def get_distances(self) -> List[Tuple[float, float]]:
+    def get_datapoint_by_distance(self, distance: ValueErrorPair[float]) -> Datapoint:
+        """
+        Get a datapoint by its distance.
+        This function will raise an error if the datapoint is not found.
+        """
+        if hash(distance.value) not in self.elements:
+            raise ValueError(f"Datapoint with distance {distance.value} not found.")
+
+        return self.elements[hash(distance.value)]
+
+    def get_distances(self) -> List[ValueErrorPair[float]]:
         return list(
             map(
-                lambda datapoint: (datapoint.distance, datapoint.distance_error),
+                lambda datapoint: datapoint.distance,
                 self.elements.values(),
             )
         )
 
-    def get_calibrations(self) -> List[Tuple[float, float]]:
+    def get_calibrations(self) -> List[ValueErrorPair[float]]:
         return list(
             map(
-                lambda datapoint: (datapoint.calibration, datapoint.calibration_error),
-                self.elements.values(),
+                lambda datapoint: coalesce(datapoint.calibration),
+                self.filter(
+                    lambda datapoint: datapoint.calibration is not None
+                ).elements.values(),
             )
         )
 
-    def get_shifted_intensities(self) -> List[Tuple[float, float]]:
+    def get_shifted_intensities(self) -> List[ValueErrorPair[float]]:
         return list(
             map(
-                lambda datapoint: (
-                    datapoint.shifted_intensity,
-                    datapoint.shifted_intensity_error,
-                ),
-                self.elements.values(),
+                lambda datapoint: coalesce(datapoint.shifted_intensity),
+                self.filter(
+                    lambda datapoint: datapoint.shifted_intensity is not None
+                ).elements.values(),
             )
         )
 
-    def get_unshifted_intensities(self) -> List[Tuple[float, float]]:
+    def get_unshifted_intensities(self) -> List[ValueErrorPair[float]]:
         return list(
             map(
-                lambda datapoint: (
-                    datapoint.unshifted_intensity,
-                    datapoint.unshifted_intensity_error,
-                ),
-                self.elements.values(),
+                lambda datapoint: coalesce(datapoint.unshifted_intensity),
+                self.filter(
+                    lambda datapoint: datapoint.unshifted_intensity is not None
+                ).elements.values(),
             )
         )
 
-    def get_feeding_shifted_intensities(self) -> List[Tuple[float, float]]:
+    def get_feeding_shifted_intensities(self) -> List[ValueErrorPair[float]]:
         return list(
             map(
-                lambda datapoint: (
-                    datapoint.feeding_shifted_intensity,
-                    datapoint.feeding_shifted_intensity_error,
-                ),
-                self.elements.values(),
+                lambda datapoint: coalesce(datapoint.feeding_shifted_intensity),
+                self.filter(
+                    lambda datapoint: datapoint.shifted_intensity is not None
+                ).elements.values(),
             )
         )
 
-    def get_feeding_unshifted_intensities(self) -> List[Tuple[float, float]]:
+    def get_feeding_unshifted_intensities(self) -> List[ValueErrorPair[float]]:
         return list(
             map(
-                lambda datapoint: (
-                    datapoint.feeding_unshifted_intensity,
-                    datapoint.feeding_unshifted_intensity_error,
-                ),
-                self.elements.values(),
+                lambda datapoint: coalesce(datapoint.feeding_unshifted_intensity),
+                self.filter(
+                    lambda datapoint: datapoint.shifted_intensity is not None
+                ).elements.values(),
             )
         )
