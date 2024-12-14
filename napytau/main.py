ed309e3edf166ad2be336e72dccd4b1b068fafd5
Benedikt Brunner <122370755+Benedikt-Brunner@users.noreply.github.com>
2024-12-14 01:30:14 +0100

    this should work

--- napytau/main.py
+++ napytau/main.py
@@ -1,10 +1,3 @@
-# Compilation mode, standalone everywhere, except on macOS there app bundle
-# nuitka-project-if: {OS} in ("Windows", "Linux", "FreeBSD"):
-#    nuitka-project: --onefile
-# nuitka-project-if: {OS} == "Darwin":
-#    nuitka-project: --standalone
-#    nuitka-project: --macos-create-app-bundle
-#
 from napytau.gui.app import init as init_gui
 from napytau.headless.headless_mockup import init as init_headless
 from napytau.cli.parser import parse_cli_arguments
