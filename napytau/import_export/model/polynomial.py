dd843ce84fb2dd6a2fc60d712167e0e6540829e1
Benedikt-Brunner <thegreencore123@gmail.com>
2024-12-20 19:29:21 +0100

    revert unintended changes

--- napytau/import_export/model/polynomial.py
+++ napytau/import_export/model/polynomial.py
@@ -9,20 +9,9 @@ class Polynomial:
     """
 
     coefficients: list[float]
-    degree: int
-
-    def __init__(self, coefficients: list[float], degree: int = 2):
-        if len(coefficients) != degree + 1:
-            raise ValueError(
-                "Number of coefficients must match the degree of the polynomial."
-            )
-
-        self.coefficients = coefficients
-        self.degree = degree
 
     def get_coefficients(self) -> list[float]:
         return self.coefficients
 
     def set_coefficients(self, coefficients: list[float]) -> None:
         self.coefficients = coefficients
-        self.degree = degree
